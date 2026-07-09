b58fa2fb29bfb6badec9cd3388cc19e74105c728
BuiQuocKhanh-VKU <khanhbq.23itb@vku.udn.vn>
2026-07-09 23:50:18 +0700

    Reference type + console

--- src/Lesson1.js
+++ src/Lesson1.js
@@ -1,4 +1,5 @@
 let age = 19;
 age = 20;
+const name = "Ten";
 
 console.log(age)
---
+++ src/Lesson3.js
@@ -0,0 +1,31 @@
+console.log("lesson 3")
+
+const car = {
+    name: "Mazda 3",
+    year: 2025,
+    Owner: "Khanh Bui",
+    true: true
+}
+console.log(car)
+
+
+
+const array = [1,2,3,4,5]
+console.log(array)
+
+
+
+let a = 2
+const b = 15, c = 10
+console.log(c%a)
+
+
+const d = a++;
+console.log("a++: "+ a)
+console.log("info: "+ a)
+// console.error("Some mistake")
+// console.warn("Test waring")
+let name = "mazda 3"
+let owner = "Khanh Bui"
+console.log("name: ", name, "owner: "+ owner)
+console.log("%c Canh bao!", "color: red; font-weight:bold")
