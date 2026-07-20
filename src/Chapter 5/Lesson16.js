b81518f42bb91a6b7c76177260866970003dd93a
BuiQuocKhanh-VKU <khanhbq.23itb@vku.udn.vn>
2026-07-20 23:39:00 +0700

    fetch

--- src/Chapter 5/Lesson16.js
+++ src/Chapter 5/Lesson16.js
@@ -1,18 +1,19 @@
-const myPromise = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("2");
-            //return
-            resolve("returned");
-        }, 2000);
-    });
-};
+// const myPromise = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("2");
+//             //return
+//             resolve("returned");
+//         }, 2000);
+//     });
+// };
 
-console.log("1");
-myPromise().then((data) => {
-    console.log("data: ", data);
-    console.log("3");
-});
-//console.log(myPromise);
+// console.log("1");
+// myPromise().then((data) => {
+//     console.log("data: ", data);
+//     console.log("3");
+// });
 
 console.log("=============================");
+const temp = fetch("http://localhost:8000/users");
+temp.then((res) => res.json()).then((data) => console.log(data));
